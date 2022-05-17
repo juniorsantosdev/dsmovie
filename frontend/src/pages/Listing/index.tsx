821e194508f3453a516fa8f68f5f65837dd7dd96
Junior Santos <josejunior.engtec@gmail.com>
2022-05-17 14:16:59 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -8,7 +8,7 @@ import { BASE_URL } from "../../utils/requests";
 
 function Listing() {
 
-    const [pageNumber] = useState(0);
+    const [pageNumber, setPageNumber] = useState(0);
 
     const [page, setPage] = useState<MoviePage>({
         content: [],
@@ -32,13 +32,15 @@ function Listing() {
 
     // forma errada
 
-
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
 
         <>
 
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
